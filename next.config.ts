c7a904d41cafba278d6b724fb4c557b9a5727299
kynaruniverse <kynaruniverse@gmail.com>
2026-06-16 20:20:16 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,9 @@
-import withPWA from 'next-pwa';
-import type { NextConfig } from 'next';
-
-const pwa = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
-  compress: true,
+  compress: true
 };
 
-export default pwa(nextConfig);
+export default nextConfig;
