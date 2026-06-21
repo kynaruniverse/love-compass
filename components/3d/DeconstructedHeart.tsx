88a05a949360cf44460f73b75663a88688366798
kynaruniverse <kynaruniverse@gmail.com>
2026-06-21 13:24:29 +0100

    update DeconstructedHeart.tsx

--- components/3d/DeconstructedHeart.tsx
+++ components/3d/DeconstructedHeart.tsx
@@ -5,6 +5,19 @@ import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import * as THREE from "three";
 
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      mesh: any;
+      sphereGeometry: any;
+      meshStandardMaterial: any;
+      ambientLight: any;
+      pointLight: any;
+      group: any;
+    }
+  }
+}
+
 interface BadgeHotspot {
   slug: string;
   position: [number, number, number];
